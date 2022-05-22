14becbf3e5f8628ef52ae362cf351a1463fbdad3
Norberto Moreno <norbermv.dev@gmail.com>
2022-05-21 21:36:04 -0500

    Add progress on  least_criticized method logic.

--- main.py
+++ main.py
@@ -24,4 +24,24 @@ def fetch_columns():
             if line_counter == 0:
                 data["column_names"] = ", ".join(row)
                 line_counter += 1
+            elif line_counter == 1:
+                data["first_row_data"] = row
+    return data
+
+@app.get("/least_criticized")
+def ten_least_criticized():
+    """Ten movies least criticized."""
+
+    with open(csv_path, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        data = {
+            "num_critic_for_reviews": [],
+        }
+
+        line_counter = 0
+        for row in csv_reader:
+            data["num_critic_for_reviews"].append(row["num_critic_for_reviews"])
+            line_counter += 1
+    data["processed_lines"] = line_counter
+
     return data

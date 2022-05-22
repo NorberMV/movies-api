9107bb851f1fd2cf39360b147ee468fc45911588
Norberto Moreno <norbermv.dev@gmail.com>
2022-05-21 19:21:34 -0500

    Adding some endpoints to fetch data from the csv file.

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+import csv
+import os.path
 
 app = FastAPI()
 
+this_dir, tail = os.path.split(__file__)
+csv_path = os.path.join(this_dir, "movie_metadata.csv")
+
+
 @app.get("/")
-def root():
-    return {"Hello": "Norbert!"}
+def home_view():
+    """The API endpoints docs."""
+    return {"This is the Movie data API!."}
+
+@app.get("/column_names")
+def fetch_columns():
+    """Fetch the data column names."""
+    with open(csv_path, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        data = dict()
+        line_counter = 0
+        for row in csv_reader:
+            if line_counter == 0:
+                data["column_names"] = ", ".join(row)
+                line_counter += 1
+    #return {"Hello": "Norber!"}
+    return data
